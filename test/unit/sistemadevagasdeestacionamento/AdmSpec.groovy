3875c524e83c59720981ea1d146abcb558075b27
Tharcio Marcilio <tharcio.0897@gmail.com>
2017-05-30 01:44:53 -0300

    Add files via upload

---
+++ test/unit/sistemadevagasdeestacionamento/AdmSpec.groovy
@@ -0,0 +1,20 @@
+package sistemadevagasdeestacionamento
+
+import grails.test.mixin.TestFor
+import spock.lang.Specification
+
+/**
+ * See the API for {@link grails.test.mixin.domain.DomainClassUnitTestMixin} for usage instructions
+ */
+@TestFor(Adm)
+class AdmSpec extends Specification {
+
+    def setup() {
+    }
+
+    def cleanup() {
+    }
+
+    void "test something"() {
+    }
+}
